d470e042b9579997e0749fe19607f88171f0cde0
Ahmed Soliman <ahmed.abdelazim911@gmail.com>
2024-02-24 23:28:54 +0200

    add build

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
